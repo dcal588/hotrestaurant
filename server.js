d50306a7960a4fc5c631c6f82cd05fe6eccf3283
Erika <eyardumian@gmail.com>
2017-09-19 20:45:04 -0700

    underscores

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get("/", function(req,res)
   });
 
   app.get("/tables", function(req, res) {
-    res.sendfile(path.join(__dirname, "tables.html"));
+    res.sendfile(path.join(__dirname, "tables.html"))
   });
 
   app.get("/reserve", function(req, res) {
